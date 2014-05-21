8ab0be7114f542fa8ea49710bd915cda14500e15
Jan Korous <jan.korous@nic.cz>
2014-05-21 10:38:50 +0200

    Ticket #11100 - In case no contact was found

--- rdap_rest/whois.py
+++ rdap_rest/whois.py
@@ -30,67 +30,72 @@ def contact_to_dict(struct):
     self_link = cz_nic_rdap_url_tmp % {"handle": struct.handle}
     cz_nic_unix_whois_url = 'whois.nic.cz'
 
-    result = {
-      "rdapConformance" : ["rdap_level_0"],
-      "handle": struct.handle,
-      "vcardArray":[
-        "vcard",
-        [
-          ["version", {}, "text", "4.0"],
-          ["fn", {}, "text", struct.name],
-          ["org", {}, "text", struct.organization],
-          ["adr",
-            { "type":"official" },
-            "text",
+    if struct is None:
+        result = {
+          "rdapConformance" : ["rdap_level_0"]
+        }    
+    else:
+        result = {
+          "rdapConformance" : ["rdap_level_0"],
+          "handle": struct.handle,
+          "vcardArray":[
+            "vcard",
             [
-              '', # P. O. BOX
-              struct.address.street1,
-              struct.address.street2,
-              struct.address.street3,
-              struct.address.city,
-              struct.address.stateorprovince,
-              struct.address.postalcode,
-              struct.address.country_code
+              ["version", {}, "text", "4.0"],
+              ["fn", {}, "text", struct.name],
+              ["org", {}, "text", struct.organization],
+              ["adr",
+                { "type":"official" },
+                "text",
+                [
+                  '', # P. O. BOX
+                  struct.address.street1,
+                  struct.address.street2,
+                  struct.address.street3,
+                  struct.address.city,
+                  struct.address.stateorprovince,
+                  struct.address.postalcode,
+                  struct.address.country_code
+                ]
+              ],
+              ["tel",
+                { "type":["official"] },
+                "uri", "tel:"+ struct.phone
+              ],
+              ["email",
+                { "type":"official" },
+                "text", struct.email
+              ],
             ]
           ],
-          ["tel",
-            { "type":["official"] },
-            "uri", "tel:"+ struct.phone
+          "status": struct.statuses,
+          "links":[
+            {
+              "value": self_link,
+              "rel":"self",
+              "href": self_link,
+              "type":"application/rdap+json"
+            }
           ],
-          ["email",
-            { "type":"official" },
-            "text", struct.email
-          ],
-        ]
-      ],
-      "status": struct.statuses,
-      "links":[
-        {
-          "value": self_link,
-          "rel":"self",
-          "href": self_link,
-          "type":"application/rdap+json"
+          "port43": cz_nic_unix_whois_url,
+          "events":[
+            {
+              "eventAction": "created",
+              "eventDate": unwrap_datetime(struct.created),
+              "eventActor": struct.creating_registrar_handle
+            }
+          ]
         }
-      ],
-      "port43": cz_nic_unix_whois_url,
-      "events":[
-        {
-          "eventAction": "created",
-          "eventDate": unwrap_datetime(struct.created),
-          "eventActor": struct.creating_registrar_handle
-        }
-      ]
-    }
-    if struct.changed is not None:
-        result['events'].append({
-            "eventAction": 'last_update',
-            "eventDate": unwrap_datetime(struct.changed)
-        })
-    if struct.last_transfer is not None:
-        result['events'].append({
-            "eventAction": 'last_transfer',
-            "eventDate": unwrap_datetime(struct.last_transfer)
-        })
+        if struct.changed is not None:
+            result['events'].append({
+                "eventAction": 'last_update',
+                "eventDate": unwrap_datetime(struct.changed)
+            })
+        if struct.last_transfer is not None:
+            result['events'].append({
+                "eventAction": 'last_transfer',
+                "eventDate": unwrap_datetime(struct.last_transfer)
+            })
 
     logging.debug(result)
     return result
