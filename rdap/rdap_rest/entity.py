5e3e9b9591b94d170390531baa0729ba802377d3
Jan Korous <jan.korous@nic.cz>
2014-08-20 16:53:17 +0200

    Ticket #11653 - Don't show data if contact is not linked

--- rdap/rdap_rest/entity.py
+++ rdap/rdap_rest/entity.py
@@ -18,82 +18,99 @@ def contact_to_dict(struct):
         result = None
     else:
         self_link = settings.RDAP_ENTITY_URL_TMPL  % {"handle": struct.handle}
+        if not "linked" in struct.statuses:
+            result = {
+                "rdapConformance" : ["rdap_level_0"],
+                "handle": struct.handle,
+                "links": [
+                    {
+                        "value": self_link,
+                        "rel":"self",
+                        "href": self_link,
+                        "type":"application/rdap+json",
+                    },
+                ],
+                "port43": settings.UNIX_WHOIS_HOST,
+                "remarks":[
+                    { "description":[ "Omitting data because contact is not linked to any registry object."] }
+                ],
+            }
+        else:
+            vcard = [["version", {}, "text", "4.0"]]
 
-        vcard = [["version", {}, "text", "4.0"]]
-
-        if struct.name is not None:
-            vcard.append(["fn", {}, "text", struct.name])
-        if struct.organization is not None:
-            vcard.append(["org", {}, "text", struct.organization])
-        if struct.address is not None:
-            vcard.append(
-                [
-                    "adr",
-                    {"type": "official"},
-                    "text",
+            if struct.name is not None:
+                vcard.append(["fn", {}, "text", struct.name])
+            if struct.organization is not None:
+                vcard.append(["org", {}, "text", struct.organization])
+            if struct.address is not None:
+                vcard.append(
                     [
-                      '',  # P. O. BOX
-                      struct.address.street1,
-                      struct.address.street2,
-                      struct.address.street3,
-                      struct.address.city,
-                      struct.address.stateorprovince,
-                      struct.address.postalcode,
-                      struct.address.country_code,
+                        "adr",
+                        {"type": "official"},
+                        "text",
+                        [
+                          '',  # P. O. BOX
+                          struct.address.street1,
+                          struct.address.street2,
+                          struct.address.street3,
+                          struct.address.city,
+                          struct.address.stateorprovince,
+                          struct.address.postalcode,
+                          struct.address.country_code,
+                        ]
                     ]
-                ]
-            )
-        if struct.phone is not None:
-            vcard.append(
-                ["tel", {"type": ["official", "voice"]}, "uri", "tel:%s" % struct.phone]
-            )
-        if struct.fax is not None:
-            vcard.append(
-                ["tel", {"type": ["official", "fax"]}, "uri", "tel:%s" % struct.fax]
-            )
-        if struct.email is not None:
-            vcard.append(
-                ["email", {"type": "official"}, "text", struct.email]
-            )
+                )
+            if struct.phone is not None:
+                vcard.append(
+                    ["tel", {"type": ["official", "voice"]}, "uri", "tel:%s" % struct.phone]
+                )
+            if struct.fax is not None:
+                vcard.append(
+                    ["tel", {"type": ["official", "fax"]}, "uri", "tel:%s" % struct.fax]
+                )
+            if struct.email is not None:
+                vcard.append(
+                    ["email", {"type": "official"}, "text", struct.email]
+                )
 
-        result = {
-            "rdapConformance" : ["rdap_level_0"],
-            "handle": struct.handle,
-            "vcardArray": ["vcard", vcard],
-            "status": struct.statuses,
-            "links": [
-                {
-                    "value": self_link,
-                    "rel":"self",
-                    "href": self_link,
-                    "type":"application/rdap+json",
-                },
-            ],
-            "port43": settings.UNIX_WHOIS_HOST,
-            "events": [
-                {
-                    "eventAction": "registration",
-                    "eventDate": unwrap_datetime(struct.created),
-                    "eventActor": struct.creating_registrar_handle,
-                }
-            ],
-            "entities": [
-                {
-                    "handle": struct.sponsoring_registrar_handle,
-                    "roles": ["registrar"],
-                },
-            ],
-        }
-        if struct.changed is not None:
-            result['events'].append({
-                "eventAction": 'last changed',
-                "eventDate": unwrap_datetime(struct.changed),
-            })
-        if struct.last_transfer is not None:
-            result['events'].append({
-                "eventAction": 'transfer',
-                "eventDate": unwrap_datetime(struct.last_transfer),
-            })
+            result = {
+                "rdapConformance" : ["rdap_level_0"],
+                "handle": struct.handle,
+                "vcardArray": ["vcard", vcard],
+                "status": struct.statuses,
+                "links": [
+                    {
+                        "value": self_link,
+                        "rel":"self",
+                        "href": self_link,
+                        "type":"application/rdap+json",
+                    },
+                ],
+                "port43": settings.UNIX_WHOIS_HOST,
+                "events": [
+                    {
+                        "eventAction": "registration",
+                        "eventDate": unwrap_datetime(struct.created),
+                        "eventActor": struct.creating_registrar_handle,
+                    }
+                ],
+                "entities": [
+                    {
+                        "handle": struct.sponsoring_registrar_handle,
+                        "roles": ["registrar"],
+                    },
+                ],
+            }
+            if struct.changed is not None:
+                result['events'].append({
+                    "eventAction": 'last changed',
+                    "eventDate": unwrap_datetime(struct.changed),
+                })
+            if struct.last_transfer is not None:
+                result['events'].append({
+                    "eventAction": 'transfer',
+                    "eventDate": unwrap_datetime(struct.last_transfer),
+                })
 
     logging.debug(result)
     return result
