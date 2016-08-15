ccd675fb07ea19251f3648fe516a5e5a1bde302e
Lena (zansorgova) <zuzana.ansorgova@nic.cz>
2016-08-15 14:35:28 +0200

    Ticket #15971 - Added basic config.comments and changed order

--- settings_rdap/rdap_cfg.py
+++ settings_rdap/rdap_cfg.py
@@ -1,11 +1,18 @@
 """
 Django settings for rdap project.
 """
+###############################################################################
+#                      RDAP Server Configuration File                         #
+###############################################################################
+
+## Django and Django REST Settings ############################################
+#
+# Note: Refer to Django and Django REST framework documentation for description.
+
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 SECRET_KEY = 'SecretKey'
-
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -16,7 +23,7 @@ USE_TZ = True
 USE_I18N = True
 USE_L10N = True
 
-# Must be present when django-guardian (dep. for restframework) is installed
+# Must be present when django-guardian (dep. for the REST framework) is installed
 ANONYMOUS_USER_ID = -1
 
 INSTALLED_APPS = (
@@ -32,27 +39,6 @@ MIDDLEWARE_CLASSES = (
 
 STATIC_URL = '/static/'
 ROOT_URLCONF = 'rdap.urls'
-
-# CORBA CONFIGURATION
-CORBA_IDL_ROOT_PATH = ''
-CORBA_IDL_WHOIS_FILENAME = 'Whois2.idl'
-CORBA_IDL_LOGGER_FILENAME = 'Logger.idl'
-CORBA_NS_HOST_PORT = ''
-CORBA_NS_CONTEXT = 'fred'
-CORBA_EXPORT_MODULES = ['Registry']
-
-# WHOIS URLS CONFIGURATION
-RDAP_ROOT_URL = ''
-RDAP_DOMAIN_URL_TMPL        = RDAP_ROOT_URL + "/domain/%(handle)s"
-RDAP_ENTITY_URL_TMPL        = RDAP_ROOT_URL + "/entity/%(handle)s"
-RDAP_NAMESERVER_URL_TMPL    = RDAP_ROOT_URL + "/nameserver/%(handle)s"
-RDAP_NSSET_URL_TMPL         = RDAP_ROOT_URL + "/fred_nsset/%(handle)s"
-RDAP_KEYSET_URL_TMPL        = RDAP_ROOT_URL + "/fred_keyset/%(handle)s"
-UNIX_WHOIS_HOST = ''
-
-# etc
-DNS_MAX_SIG_LIFE = 1209600
-DISCLAIMER_FILE = ''
 
 LOG_FILENAME = ''
 LOGGING = {
@@ -87,3 +73,42 @@ LOGGING = {
         },
     },
 }
+
+
+
+## RDAP Server Settings #######################################################
+
+### CORBA configuration
+# Path to the directory with IDL files (absolute path recommended)
+CORBA_IDL_ROOT_PATH = ''
+# Whois IDL file name
+CORBA_IDL_WHOIS_FILENAME = 'Whois2.idl'
+# Logger IDL file name
+CORBA_IDL_LOGGER_FILENAME = 'Logger.idl'
+# Name service address (host[:port])
+CORBA_NS_HOST_PORT = ''
+# Name service context
+CORBA_NS_CONTEXT = 'fred'
+# Modules exported for RDAP - Don't change this!
+CORBA_EXPORT_MODULES = ['Registry']
+
+### WHOIS URLs configuration
+# Base of URLs
+RDAP_ROOT_URL = ''
+# URL path for domain queries
+RDAP_DOMAIN_URL_TMPL        = RDAP_ROOT_URL + "/domain/%(handle)s"
+# URL path for entity queries
+RDAP_ENTITY_URL_TMPL        = RDAP_ROOT_URL + "/entity/%(handle)s"
+# URL path for nameserver queries
+RDAP_NAMESERVER_URL_TMPL    = RDAP_ROOT_URL + "/nameserver/%(handle)s"
+# URL path for NSSET queries
+RDAP_NSSET_URL_TMPL         = RDAP_ROOT_URL + "/fred_nsset/%(handle)s"
+# URL path for keyset queries
+RDAP_KEYSET_URL_TMPL        = RDAP_ROOT_URL + "/fred_keyset/%(handle)s"
+# Unix Whois server address
+UNIX_WHOIS_HOST = ''
+
+# Maximum signature lifetime (seconds)
+DNS_MAX_SIG_LIFE = 1209600
+# File that is included in each response
+DISCLAIMER_FILE = ''
