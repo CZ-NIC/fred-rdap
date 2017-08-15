40fd4f9c8396a2fbb1c37db6488d37f715798dbd
Vlastimil Zíma <vlastimil.zima@nic.cz>
2017-08-15 12:39:23 +0200

    Ticket #19731 - Remove configuration from install

---
+++ examples/rdap_cfg.py
@@ -0,0 +1,73 @@
+"""
+Example django settings for RDAP project.
+
+Do not use on production - DEBUG settings are turned on.
+"""
+###############################################################################
+#                      RDAP Server Configuration File                         #
+###############################################################################
+
+# ## Django Settings ##########################################################
+#
+# Note: Refer to the Django documentation for a description.
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+SECRET_KEY = 'SecretKey'
+DEBUG = True
+ALLOWED_HOSTS = ['*']
+
+TIME_ZONE = 'UTC'
+USE_TZ = True
+
+INSTALLED_APPS = ()
+
+MIDDLEWARE = (
+    'django.middleware.common.CommonMiddleware',
+)
+
+ROOT_URLCONF = 'rdap.urls'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'simple': {'format': '%(asctime)s - %(name)s - %(levelname)-8s - %(message)s'},
+    },
+    'handlers': {
+        'console': {'level': 'DEBUG',
+                    'class': 'logging.StreamHandler',
+                    'formatter': 'simple'},
+    },
+    'loggers': {
+        '': {'handlers': ['console'],
+             'level': 'DEBUG'},
+    },
+}
+
+
+# ## RDAP Server Settings #####################################################
+
+# #### CORBA configuration
+# Path to the directory with IDL files (absolute path recommended)
+CORBA_IDL_ROOT_PATH = ''
+# Whois IDL file name
+CORBA_IDL_WHOIS_FILENAME = 'Whois2.idl'
+# Logger IDL file name
+CORBA_IDL_LOGGER_FILENAME = 'Logger.idl'
+# Naming service address (host[:port])
+CORBA_NS_HOST_PORT = ''
+# Naming service context
+CORBA_NS_CONTEXT = 'fred'
+
+# #### WHOIS URLs configuration
+# Base of URLs
+RDAP_ROOT_URL = ''
+# Unix Whois server address
+UNIX_WHOIS_HOST = ''
+
+# Maximum signature lifetime (seconds) used in secureDNS
+DNS_MAX_SIG_LIFE = 1209600
+# File path of the disclaimer notice that is included in each response
+DISCLAIMER_FILE = ''
--- setup.py
+++ setup.py
@@ -1,82 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import os
-
-from freddist.command.install import install
 from freddist.core import setup
-from freddist.util import findall, find_data_files, find_packages
 
 
 PROJECT_NAME = 'fred-rdap'
-
-
-class ModuleInstall(install):
-
-    user_options = install.user_options + [
-        ('host=', None,
-         'RDAP host'),
-        ('port=', None,
-         'RDAP port'),
-        ('rdappath=', None,
-         'RDAP path'),
-        ('unixwhoishost=', None,
-         'host of related unix whois protocol'),
-        ('nshostport=', None,
-         'host:port of CORBA name service'),
-        ('wsgirundir=', None,
-         'Directory for WSGI socket'),
-        ('disclaimerfile=', None,
-         'File with disclaimer')
-    ]
-
-    def initialize_options(self):
-        install.initialize_options(self)
-        self.host = "localhost"
-        self.port = None
-        self.unixwhoishost = None
-        self.nshostport = None
-        self.wsgirundir = ""
-        self.disclaimerfile = None
-        self.rdappath = "/rdap"
-
-    def update_rdap_cfg_py(self, filename):
-        content = open(filename).read()
-        content = content.replace("CORBA_IDL_ROOT_PATH = ''", "CORBA_IDL_ROOT_PATH = '" + self.expand_filename('$data/share/idl/fred') + "'")
-        if self.nshostport is not None:
-            content = content.replace("CORBA_NS_HOST_PORT = ''", "CORBA_NS_HOST_PORT = '" + self.nshostport + "'")
-        hostport = self.host
-        if self.port is not None:
-            hostport = hostport + ":" + self.port
-        content = content.replace("RDAP_ROOT_URL = ''", "RDAP_ROOT_URL = 'http://" + hostport + self.rdappath + "'")
-        if self.unixwhoishost is not None:
-            content = content.replace("UNIX_WHOIS_HOST = ''", "UNIX_WHOIS_HOST = '" + self.unixwhoishost + "'")
-        if self.disclaimerfile is not None:
-            content = content.replace("DISCLAIMER_FILE = ''", "DISCLAIMER_FILE = '" + self.disclaimerfile + "'")
-        content = content.replace("LOG_FILENAME = ''", "LOG_FILENAME = '" + self.expand_filename('$localstate/log/fred-rdap.log') + "'")
-
-        open(filename, 'w').write(content)
-        self.announce("File '%s' was updated" % filename)
-
-    def update_apache_conf(self, filename):
-        content = open(filename).read()
-        content = content.replace("INSTALL_PURELIB", self.expand_filename('$purelib/rdap'))
-        content = content.replace("WSGI_FILE", self.expand_filename('$purelib/rdap/run.wsgi'))
-        content = content.replace("RDAP_PATH", self.rdappath)
-        if self.port is not None:
-            content = "Listen " + self.port + "\n" + content
-        content = content.replace("WSGI_RUN_DIR_ROOT", self.wsgirundir)
-
-        open(filename, 'w').write(content)
-        self.announce("File '%s' was updated" % filename)
-
-    def update_run_wsgi(self, filename):
-        content = open(filename).read()
-        content = content.replace("PURELIB_DIR", self.expand_filename('$purelib'))
-        content = content.replace("PURELIB_RDAP_DIR", self.expand_filename('$purelib/rdap'))
-        content = content.replace("SYSCONF_DIR", self.expand_filename('$sysconf/fred'))
-
-        open(filename, 'w').write(content)
-        self.announce("File '%s' was updated" % filename)
 
 
 def main():
@@ -93,20 +20,7 @@ def main():
               'rdap': ['rdap/*'],
               'rdap.utils': ['rdap/utils/*'],
               'rdap.rdap_rest': ['rdap/rdap_rest/*'],
-          },
-          data_files=[
-              ('$sysconf/fred/', ['settings_rdap/rdap_cfg.py']),
-              ('$data/share/fred-rdap', ['settings_rdap/apache.conf']),
-              ('$sysconf/fred/', ['settings_rdap/rdap_disclaimer.txt']),
-              ('$purelib/rdap', ['rdap/run.wsgi'])
-          ],
-          cmdclass={'install': ModuleInstall},
-          modify_files={
-              '$sysconf/fred/rdap_cfg.py': 'update_rdap_cfg_py',
-              '$data/share/fred-rdap/apache.conf': 'update_apache_conf',
-              '$purelib/rdap/run.wsgi': 'update_run_wsgi'
-          }
-    )
+          })
 
 
 if __name__ == '__main__':
