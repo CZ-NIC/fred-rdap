6382dfecdf247a494b24ef7a6bfb1a3bac9cbf06
Vlastimil Zíma <vlastimil.zima@nic.cz>
2017-08-15 12:55:47 +0200

    Ticket #19731 - Drop freddist

--- setup.py
+++ setup.py
@@ -1,27 +1,17 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from freddist.core import setup
+from setuptools import find_packages, setup
 
 
-PROJECT_NAME = 'fred-rdap'
-
-
-def main():
-    setup(name=PROJECT_NAME,
-          description='NIC.CZ RDAP',
-          author='Jan Korous, CZ.NIC',
-          author_email='jan.korous@nic.cz',
-          url='http://www.nic.cz/',
-          license='GNU GPL',
-          platforms=['posix'],
-          long_description='CZ.NIC RDAP server',
-          packages=('rdap', 'rdap.rdap_rest', 'rdap.utils'),
-          package_data={
-              'rdap': ['rdap/*'],
-              'rdap.utils': ['rdap/utils/*'],
-              'rdap.rdap_rest': ['rdap/rdap_rest/*'],
-          })
-
-
-if __name__ == '__main__':
-    main()
+setup(name='fred-rdap',
+      description='NIC.CZ RDAP',
+      author='Jan Korous, CZ.NIC',
+      author_email='jan.korous@nic.cz',
+      url='http://www.nic.cz/',
+      license='GNU GPL',
+      platforms=['posix'],
+      long_description='CZ.NIC RDAP server',
+      packages=find_packages(),
+      include_package_data=True,
+      install_requires=['django', 'idna', 'fred-pylogger', 'pyfco'],
+      )
