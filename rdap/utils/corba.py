e80a72c8dbccee2669d1803b72a40fa6d7eda792
Vlastimil Zíma <vlastimil.zima@nic.cz>
2017-08-18 11:52:40 +0200

    Ticket #19776 - Define pyfco Corba objects

--- rdap/utils/corba.py
+++ rdap/utils/corba.py
@@ -3,11 +3,14 @@
 import logging
 import sys
 import types
+import os.path
 
 import CosNaming
 import omniORB
 from django.conf import settings
+from django.utils.functional import SimpleLazyObject
 from omniORB import CORBA, importIDL
+from pyfco import CorbaNameServiceClient
 
 
 # own exceptions
@@ -141,3 +144,36 @@ def get_corba_for_module(mod_name):
     else:
         create_corba_for_module(mod_name)
         return corbas.get(mod_name)
+
+
+def _get_registry_module():
+    """Return `Registry` module."""
+    try:
+        import Registry
+    except ImportError:
+        Registry = None
+
+    if not hasattr(Registry, 'Whois'):
+        omniORB.importIDL(os.path.join(settings.CORBA_IDL_ROOT_PATH, settings.CORBA_IDL_WHOIS_FILENAME))
+        import Registry
+    return Registry
+
+
+def _get_ccreg_module():
+    """Return `ccReg` module."""
+    try:
+        import ccReg
+    except ImportError:
+        ccReg = None
+
+    if not hasattr(ccReg, 'Logger'):
+        omniORB.importIDL(os.path.join(settings.CORBA_IDL_ROOT_PATH, settings.CORBA_IDL_LOGGER_FILENAME))
+        import ccReg
+    return ccReg
+
+
+REGISTRY_MODULE = SimpleLazyObject(_get_registry_module)
+CCREG_MODULE = SimpleLazyObject(_get_ccreg_module)
+_CORBA = CorbaNameServiceClient(host_port=settings.CORBA_NS_HOST_PORT, context_name=settings.CORBA_NS_CONTEXT)
+WHOIS = SimpleLazyObject(lambda: _CORBA.get_object('Whois2', REGISTRY_MODULE.Whois.WhoisIntf))
+LOGGER = SimpleLazyObject(lambda: _CORBA.get_object('Logger', CCREG_MODULE.Logger))
