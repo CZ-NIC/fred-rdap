40a7701e1e5525084388ab660610d6100ad89ba3
Vlastimil Zíma <vlastimil.zima@nic.cz>
2017-08-24 15:16:29 +0200

    Ticket #19784 - Drop py_logging module

--- rdap/utils/py_logging.py
+++
@@ -1,6 +0,0 @@
-from django.utils.functional import SimpleLazyObject
-from pylogger.corbalogger import Logger
-
-from .corba import CCREG_MODULE, LOGGER
-
-py_logger_obj = SimpleLazyObject(lambda: Logger(LOGGER, CCREG_MODULE))
