21cabc96659a962610515529a0709cb904d52974
Vlastimil Zíma <vlastimil.zima@nic.cz>
2018-02-05 15:44:48 +0100

    Ticket #21107 - Add conformance tests into CI

---
+++ test_cfg/conformance/settings.py
@@ -0,0 +1,12 @@
+DEBUG = False
+ALLOWED_HOSTS = ['*']
+INSTALLED_APPS = ()
+ROOT_URLCONF = 'rdap.urls'
+SECRET_KEY = 'SECRET'
+
+CORBA_NS_HOST_PORT = 'localhost'
+CORBA_NS_CONTEXT = 'fred'
+RDAP_ROOT_URL = 'http://localhost:8000'
+DNS_MAX_SIG_LIFE = 1209600
+DISCLAIMER_FILE = ''
+UNIX_WHOIS_HOST = 'whois.nic.cz'
