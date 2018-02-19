40daeb3bbbf738fb345c210557f9463128f3d797
Vlastimil Zíma <vlastimil.zima@nic.cz>
2018-02-19 10:27:36 +0100

    Ticket #21257 - Add CORS headers to conformance CI [WIP]

--- test_cfg/conformance/settings.py
+++ test_cfg/conformance/settings.py
@@ -1,6 +1,22 @@
+class CorsMiddleware(object):
+    """Middleware to add CORS header to every response.
+
+    Workaround for the bug reported to RDAP conformance tests https://github.com/APNIC-net/rdap-conformance/issues/41.
+    """
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        response['Access-Control-Allow-Origin'] = '*'
+        return response
+
+
 DEBUG = False
 ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = ()
+MIDDLEWARE = ['settings.CorsMiddleware',
+              'django.middleware.common.CommonMiddleware']
 ROOT_URLCONF = 'rdap.urls'
 SECRET_KEY = 'SECRET'
 
