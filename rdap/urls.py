4cef72c94707bbe9017a0bffaf7eaef7e01d6ad6
Vlastimil Zíma <vlastimil.zima@nic.cz>
2019-09-02 10:42:35 +0200

    Ticket #25792 - Drop case insensitivity from URLs

--- rdap/urls.py
+++ rdap/urls.py
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2014-2018  CZ.NIC, z. s. p. o.
+# Copyright (C) 2014-2019  CZ.NIC, z. s. p. o.
 #
 # This file is part of FRED.
 #
@@ -25,26 +25,26 @@ from rdap.rdap_rest.whois import get_contact_by_handle, get_domain_by_handle, ge
 from rdap.views import FqdnObjectView, HelpView, ObjectView, UnsupportedView
 
 urlpatterns = [
-    url(r'^(?i)entity/(?P<handle>.+)$',
+    url(r'^entity/(?P<handle>.+)$',
         ObjectView.as_view(getter=get_contact_by_handle, request_type='EntityLookup'),
         name='entity-detail'),
-    url(r'^(?i)domain/(?P<handle>.+)$',
+    url(r'^domain/(?P<handle>.+)$',
         FqdnObjectView.as_view(getter=get_domain_by_handle, request_type='DomainLookup'),
         name='domain-detail'),
-    url(r'^(?i)nameserver/(?P<handle>.+)$',
+    url(r'^nameserver/(?P<handle>.+)$',
         FqdnObjectView.as_view(getter=get_nameserver_by_handle, request_type='NameserverLookup'),
         name='nameserver-detail'),
-    url(r'^(?i)fred_nsset/(?P<handle>.+)$',
+    url(r'^fred_nsset/(?P<handle>.+)$',
         ObjectView.as_view(getter=get_nsset_by_handle, request_type='NSSetLookup'),
         name='nsset-detail'),
-    url(r'^(?i)fred_keyset/(?P<handle>.+)$',
+    url(r'^fred_keyset/(?P<handle>.+)$',
         ObjectView.as_view(getter=get_keyset_by_handle, request_type='KeySetLookup'),
         name='keyset-detail'),
-    url(r'^(?i)autnum/.+$', UnsupportedView.as_view()),
-    url(r'^(?i)ip/.+$', UnsupportedView.as_view()),
-    url(r'^(?i)domains$', UnsupportedView.as_view()),
-    url(r'^(?i)nameservers$', UnsupportedView.as_view()),
-    url(r'^(?i)entities$', UnsupportedView.as_view()),
-    url(r'^(?i)help$', HelpView.as_view(), name='help'),
+    url(r'^autnum/.+$', UnsupportedView.as_view()),
+    url(r'^ip/.+$', UnsupportedView.as_view()),
+    url(r'^domains$', UnsupportedView.as_view()),
+    url(r'^nameservers$', UnsupportedView.as_view()),
+    url(r'^entities$', UnsupportedView.as_view()),
+    url(r'^help$', HelpView.as_view(), name='help'),
     url(r'.*', UnsupportedView.as_view(status=400)),
 ]
